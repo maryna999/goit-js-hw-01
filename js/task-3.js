1eb15063497bfc9507affef8bd64e22d498327ad
Maryna <mishka999@ukr.net>
2024-05-14 16:56:03 +0300

    add 3 task

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,11 @@
 function getElementWidth(content, padding, border) {
-    const boxWidth = content + padding * 2 + border * 2;
-    return boxWidth;
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    return totalWidth;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
